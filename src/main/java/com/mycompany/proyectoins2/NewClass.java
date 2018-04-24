1a8f1986de423425ddf8fa81ab99b4fe5490382a
user <user@Carlos>
2018-04-23 23:12:55 -0500

    Prueba 1

---
+++ src/main/java/com/mycompany/proyectoins2/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * Carlos Steven Portilla Botero
+ * 1015473300
+ * Ing. Sistemas y telecomunicaciones 
+ *   * 
+ */
+package com.mycompany.proyectoins2;
+
+/**
+ *
+ * @author user
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("servi");
+    }
+}
