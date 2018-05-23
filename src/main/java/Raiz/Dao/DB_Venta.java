766c5901545b6cf10b53b181d8cba15764bb80b3
user <user@Carlos>
2018-05-22 20:59:56 -0500

    Casi full

--- src/main/java/Raiz/Dao/DB_Venta.java
+++ src/main/java/Raiz/Dao/DB_Venta.java
@@ -6,12 +6,19 @@
  */
 package Raiz.Dao;
 
+import Raiz.Album;
 import Raiz.Artista;
+import Raiz.Cancion;
+import Raiz.ListaAlbum;
+import Raiz.ListaCancion;
 import Raiz.Ventas;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
@@ -95,6 +102,154 @@ public class DB_Venta {
             return false;
         }
         return true;
+    }
+
+    public int buscarVentasCan(int a) throws SQLException {
+        ArrayList<ListaCancion> arrayList = new ArrayList<ListaCancion>();
+        ListaCancion listaCancion = new ListaCancion();
+        int res = 0;
+        try {
+            // create the java statement
+
+            Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            String query = "SELECT  * FROM  Ventas  where idCancion = " + a;
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                // iterate through the java resultset
+                res += rs.getInt("Cantidad");
+            }
+            // print the results
+            st.close();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Failed to make update!");
+            e.printStackTrace();
+            return 0;
+        }
+
+        return res;
+    }
+
+    public int buscarVentasCanAntesDe(int a, long b) throws SQLException {
+
+        int res = 0;
+        try {
+            // create the java statement
+
+            Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            String query = "SELECT  * FROM  Ventas  where idCancion = " + a + " and Fecha < " + b;
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                // iterate through the java resultset
+                res += rs.getInt("Cantidad");
+            }
+            // print the results
+            st.close();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Failed to make update!");
+            e.printStackTrace();
+            return 0;
+        }
+
+        return res;
+    }
+
+    public int buscarVentasAlbAntesDe(String a, long b) throws SQLException {
+
+        int res = 0;
+        try {
+            // create the java statement
+
+            Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            String query = "SELECT  * FROM  Ventas  where NombreAlbum = " + a + " and Fecha < " + b;
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                // iterate through the java resultset
+                res += rs.getInt("Cantidad");
+            }
+            // print the results
+            st.close();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Failed to make update!");
+            e.printStackTrace();
+            return 0;
+        }
+
+        return res;
+    }
+
+    public ArrayList<Cancion> buscarCanciones(Long a) throws SQLException {
+        ArrayList<Cancion> arrayList = new ArrayList<Cancion>();
+        ListaCancion listaCancion = new ListaCancion();
+        try {
+            // create the java statement
+
+            Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            String query = "SELECT  * FROM  Ventas  where NombreAlbum = null and Fecha <" + a;
+            ResultSet rs = st.executeQuery(query);
+            DB_Cancion db_Cancion = new DB_Cancion();
+            while (rs.next()) {
+                // iterate through the java resultset
+                arrayList.add(db_Cancion.buscarCancionPorId(rs.getInt("idCancion"), a));
+
+            }
+            // print the results
+            st.close();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Failed to make update!");
+            e.printStackTrace();
+            return null;
+        }
+
+        return arrayList;
+    }
+
+    public ArrayList<Album> buscarAlbumes(Long a) throws SQLException {
+        ArrayList<Album> arrayList = new ArrayList<Album>();
+
+        try {
+            // create the java statement
+
+            Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            String query = "SELECT  * FROM  Ventas  where idCancion=0 and Fecha <" + a;
+            ResultSet rs = st.executeQuery(query);
+            DB_Album db_Album = new DB_Album();
+            while (rs.next()) {
+                // iterate through the java resultset
+
+                arrayList.add(db_Album.buscarCanciones(rs.getString("NombreAlbum")));
+            }
+            // print the results
+            st.close();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Failed to make update!");
+            e.printStackTrace();
+            return null;
+        }
+
+        return arrayList;
     }
 
     public void desconectar() {
